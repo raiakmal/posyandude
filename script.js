40fb31f86002f5518e837a3a528674b1b5812f9b
Muhammad Rai Akmal <muhammadrai0504@gmail.com>
2024-05-30 17:34:53 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,18 @@
+// Scroll
+document.querySelector('button').addEventListener('click', () => {
+    document.querySelector('.md\\:hidden').classList.toggle('hidden');
+  });
+
+// Maps
+// Initialize the map and set its view to the specified coordinates and zoom level
+var map = L.map('map').setView([-7.366844, 108.206130], 13);
+
+// Add OpenStreetMap tiles to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Add a marker at the specified coordinates
+L.marker([-7.366844, 108.206130]).addTo(map)
+    .bindPopup('Mugarsari, Kec. Tamansari, Kota Tasikmalaya, Jawa Barat')
+    .openPopup();
